8aaf99412d4c428df97c3694cc3dc233070a5423
wayne <wayneli058@gmail.com>
2022-05-24 14:00:40 +0800

    Done.

--- app/src/main/java/com/example/sa_vendingmachine_app/MainActivity.java
+++ app/src/main/java/com/example/sa_vendingmachine_app/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.sa_vendingmachine_app;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -12,6 +18,12 @@ import com.example.sa_vendingmachine_app.Model.DBHandlerThread;
 import com.example.sa_vendingmachine_app.Model.ExecuteSQL;
 import com.example.sa_vendingmachine_app.Model.SQLExecuteTypeEnum;
 import com.example.sa_vendingmachine_app.databinding.ActivityMainBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -21,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "Debugger";
 
+    private GoogleSignInOptions googleSignInOptions;
+
+    private GoogleSignInClient googleSignInClient;
+
     private ActivityMainBinding UI;
 
     @Override
@@ -29,62 +45,47 @@ public class MainActivity extends AppCompatActivity {
         UI = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(UI.getRoot());
 
-        // debugger
-        UI.accontEditTextText.setText("test");
-        UI.passwordEditTextText.setText("test");
-
         // 綁定畫面
         bindViewListener();
+
+        // 初始化Google登入
+        initGoogleSignIn();
+
+        googleSignInClient.signOut();
     }
 
     private void bindViewListener() {
-        UI.loginButton.setOnClickListener(v -> {
-            String account = UI.accontEditTextText.getText().toString();
-
-            if (account.isEmpty()) {
-                Toast.makeText(this, "Account can't be empty", Toast.LENGTH_LONG).show();
-                return;
-            }
-
-            ExecuteSQL checkAccount = new ExecuteSQL();
-            checkAccount.setSql(String.format(
-                    "SELECT * FROM vending_machine.user WHERE account = '%s';", account
-            ));
-            checkAccount.setType(SQLExecuteTypeEnum.QUERY);
-            checkAccount.execute();
-
-            try {
-                if(checkAccount.getResultSet().next()) {
-                    String password = UI.passwordEditTextText.getText().toString();
-
-                    if (password.isEmpty()) {
-                        Toast.makeText(this, "Password can't be empty", Toast.LENGTH_LONG).show();
-                        return;
-                    }
-
-                    ExecuteSQL checkPassword = new ExecuteSQL();
-                    checkPassword.setSql(String.format(
-                            "SELECT password FROM vending_machine.user WHERE account = '%s';", account
-                    ));
-                    checkPassword.setType(SQLExecuteTypeEnum.QUERY);
-                    checkPassword.execute();
-
-                    if (checkPassword.getResultSet().next()) {
-                        if (Objects.equals(checkPassword.getResultSet().getString("password"), password)) {
-                            Log.e(TAG, "bindViewListener: in");
-                        }else {
-                            Toast.makeText(this, "Wrong password.", Toast.LENGTH_LONG).show();
-                        }
-                    }else {
-                        Toast.makeText(this, "Wrong password.", Toast.LENGTH_LONG).show();
-                    }
-
-                }else {
-                    Toast.makeText(this, "Wrong account.", Toast.LENGTH_LONG).show();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+        UI.loginButton.setOnClickListener(e ->{
+            googleSignInActivityResultLauncher.launch(googleSignInClient.getSignInIntent());
         });
     }
+
+    private void initGoogleSignIn() {
+        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("360842512331-7jki9ch9k4ps27t3k07aei48lpmcappj.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+    }
+
+    ActivityResultLauncher<Intent> googleSignInActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                    try {
+                        GoogleSignInAccount account = task.getResult(ApiException.class);
+                        String msg = "登入成功\nEmail："+account.getEmail()+"\nGoogle名稱：" + account.getDisplayName();
+                        Log.e(TAG, msg);
+                        Log.d(TAG, "Token: " + account.getIdToken());
+                    } catch (ApiException e) {
+                        Log.w(TAG, "Google sign in failed", e);
+                    }
+                }else {
+                    Toast.makeText(this, "Sign in fail, please retry.", Toast.LENGTH_LONG).show();
+                }
+            }
+    );
 }
